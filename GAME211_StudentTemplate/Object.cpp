02a753bcb8b053ff851ad13c69ba617da937b81d
Luthii <carolsro@gmail.com>
2024-10-03 15:47:41 -0400

    Updates to the object class

--- GAME211_StudentTemplate/Object.cpp
+++ GAME211_StudentTemplate/Object.cpp
@@ -1,17 +1,65 @@
 #include "Object.h"
 
-Object::Object(std::string filePath, SDL_Renderer* windowRender)
+Object::Object(std::string filePath_, SDL_Renderer* sceneRenderer_)
 {
+    filePath = filePath_;
+    sceneRenderer = sceneRenderer_;
+
+    TEX_object = nullptr;
+    SPR_object = nullptr;
+
 }
 
 Object::~Object()
 {
 }
 
+bool Object::OnCreate()
+{
+    SDL_Surface* SPR_object;
+    SPR_object = IMG_Load(filePath.c_str());
+
+    if (SPR_object == nullptr) {
+        std::cerr << "Can't open the image" << std::endl;
+        return false;
+    }
+
+    TEX_object = SDL_CreateTextureFromSurface(sceneRenderer, SPR_object);
+    if (TEX_object == nullptr) {
+        std::cerr << "Erro creating texture from: " << filePath << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool Object::OnDestroy()
+{
+    SDL_DestroyTexture(TEX_object);
+    TEX_object = nullptr;
+
+    return true;
+}
+
 void Object::Update(float deltaTime)
 {
 }
 
-void Object::Render(SDL_Renderer* windowRender)
+void Object::Render(SDL_Renderer* sceneRenderer)
 {
+    SDL_Rect rect;
+    Matrix4 projectionMatrix = Matrix4();//game->getProjectionMatrix();
+
+    Vec3 screenCoords = projectionMatrix * position;
+
+    // The square's x and y values represent the top left corner of 
+    // where SDL will draw the .png image.
+    // The 0.5f * w/h offset is to place the .png so that pos represents the center
+    // (Note the y axis for screen coords points downward, hence subtraction!!!!)
+    rect.x = static_cast<int>(screenCoords.x - (0.5f * SPR_object->w));
+    rect.y = static_cast<int>(screenCoords.y - (0.5f * SPR_object->h));
+    rect.w = static_cast<int>(SPR_object->w);
+    rect.h = static_cast<int>(SPR_object->h);
+
+    SDL_RenderCopyEx(sceneRenderer, TEX_object, nullptr, &rect, 0.0, nullptr, SDL_FLIP_NONE);
 }
--- GAME211_StudentTemplate/Object.h
+++ GAME211_StudentTemplate/Object.h
@@ -4,21 +4,32 @@
 #include <iostream>
 #include <string>
 
+#include "GameManager.h"
+#include "Vector.h"
+
 class Object
 {
 private:
-	SDL_Texture* SPR_object; //to start simple, only a single sprite, no animation right now
+	SDL_Texture* TEX_object; //to start simple, only a single sprite, no animation right now
+	SDL_Surface* SPR_object;
+	SDL_Renderer* sceneRenderer;
 
 	//initiate the variables of the sprite to be 16x16 pixels and scale of 1
 	int SPR_width = 16;
 	int SPR_height = 16;
 	int SPR_scale = 1;
 
-	int position[2] = { 0,0 }; //initiate position to (0,0)
+	Vec3 position = Vec3(0,0,0); //initiate position to (0,0,0) -> x = 0, y = 0, layer, z = 0
+
+	std::string filePath;
+
 
 public:
-	Object(std::string filePath, SDL_Renderer* windowRender);
+	Object(std::string filePath_, SDL_Renderer* sceneRenderer_);
 	~Object();
+
+	bool OnCreate();
+	bool OnDestroy();
 
 	void Update(float deltaTime);
 	void Render(SDL_Renderer* windowRender);

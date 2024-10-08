2609a858d1cc2c4e77bbbe737be9b74b9c2e07df
lei <lanaleigh.santos0326@gmail.com>
2024-10-07 23:02:56 -0400

    Entity, Character, Player, NPC Class

--- GAME211_StudentTemplate/Character.h
+++ GAME211_StudentTemplate/Character.h
@@ -3,23 +3,24 @@
 #define CHARACTER_H
 
 #include <string>
-#include "Body.h"
+#include "Entity.h" // to be replaced with entity
 
-class Character :public Body {
+class Character :public Entity {
 protected:
-	std::string name;
-	std::string occupation;
+	std::string name; //
+	std::string type; // Can be occupation like smith, cook, etc., alternatively monster type 
+    int health; // 100
+    int speed; // movement speed
 
 public:
     // Constructors
-	Character() : Body{}, name{ "" }, occupation{ "" } {}
-
+	Character() : Entity{}, name{ "" }, type{ "" } {}
     Character(
         const std::string& name_, 
-        const std::string& occupation_,
+        const std::string& type_,
         Vec3 pos_, Vec3 vel_, Vec3 accel_,
         float mass_, float radius_, float orientation_,
-        float rotation_, float angular_) : Body{ pos_, vel_, accel_, mass_, radius_, orientation_, rotation_, angular_ },name{ name_ }, occupation{ occupation_ } {}
+        float rotation_, float angular_) : Entity{ pos_, vel_, accel_, mass_, radius_, orientation_, rotation_, angular_ },name{ name_ }, type{ type_ } {}
 
 
     // Getters
@@ -27,5 +28,14 @@ public:
 
     // Setters
     void setName(const std::string& name_) { name = name_; }
-    void setOccupation(const std::string& occupation_) {occupation = occupation_;}
+    void setOccupation(const std::string& type_) { type = type_;}
+    void characterMove(int dx, int dy){
+        pos.x += dx * speed;
+        pos.y += dy * speed;
+    }
+    void healthCheck() {}
+    virtual void interact() {}
+
 };
+
+#endif CHARACTER_H

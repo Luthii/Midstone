7c19f42564c287ef409e43a613b83b55bd9e5bdb
Curtis <cjhouse28@gmail.com>
2024-10-10 23:49:42 -0400

    Revert "Fixed originally buggy Map and TextureManager"
    This reverts commit d1f55222d5d2408cca48f1f21f3f8a69b26338ce.

--- GAME211_StudentTemplate/Scene1.h
+++ GAME211_StudentTemplate/Scene1.h
@@ -3,7 +3,6 @@
 
 #include <MMath.h>
 #include "Scene.h"
-#include "Map.h"
 
 using namespace MATH;
 class Scene1 : public Scene {
@@ -14,8 +13,6 @@ private:
 	SDL_Renderer* renderer;	// the renderer associated with SDL window
 	Matrix4 projectionMatrix;	// set in OnCreate()
     Matrix4     inverseProjection;	// set in OnCreate()
-
-	Map* map;
 
 public:
 	// This constructor may be different from what you've seen before

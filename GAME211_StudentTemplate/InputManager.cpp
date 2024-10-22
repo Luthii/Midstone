3a33f1c04532ed8275af0541560ac11b91f9186d
Luthii <carolsro@gmail.com>
2024-10-22 18:53:54 -0400

    Fixed on key numbers
    Littler fixe on the keyboard keys accepted in the input manager

--- GAME211_StudentTemplate/InputManager.cpp
+++ GAME211_StudentTemplate/InputManager.cpp
@@ -18,10 +18,16 @@ void InputManager::Update()
 	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
 		case SDL_KEYDOWN:
+			if (event.key.keysym.sym > 320)
+				break;
+
 			keyDown[event.key.keysym.sym] = true;
 			keyUp[event.key.keysym.sym] = false;
 			break;
 		case SDL_KEYUP:
+			if (event.key.keysym.sym > 320)
+				break;
+
 			keyUp[event.key.keysym.sym] = true;
 			keyDown[event.key.keysym.sym] = false;
 			break;
